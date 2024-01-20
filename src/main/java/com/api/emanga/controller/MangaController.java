e203f580c3124d5794d945a49aa07191b7a49ed1
andreyviana <andreyviana125@gmail.com>
2024-01-20 11:57:40 -0300

    TASK: POST METHOD DOESN'T ACT AS PUT

--- src/main/java/com/api/emanga/controller/MangaController.java
+++ src/main/java/com/api/emanga/controller/MangaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.api.emanga.model.MangaModel;
 import com.api.emanga.service.MangaService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/emanga/mangas")
@@ -35,11 +38,12 @@ public class MangaController {
 
 	@PostMapping
 	public ResponseEntity<MangaModel> adicionarManga(@RequestBody MangaModel manga) {
-		return ResponseEntity.status(201).body(service.adicionarManga(manga));
+		return manga.getId() == null ? ResponseEntity.status(201).body(service.adicionarManga(manga)) 
+			: ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<Optional<MangaModel>> atualizarManga(@PathVariable("id") Integer id, @RequestBody MangaModel manga) {
+	public ResponseEntity<Optional<MangaModel>> atualizarManga(@Valid @PathVariable("id") Integer id, @RequestBody MangaModel manga) {
 		return service.atualizarManga(id, manga).isPresent() ? ResponseEntity.ok(service.atualizarManga(id, manga)) 
 			: ResponseEntity.notFound().build();
 	}
